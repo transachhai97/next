1d6027aeecdbb6ca2489e034df3b19637ce84259
HaiTS <transachhai97@gmail.com>
2020-10-01 14:22:38 +0700

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['nextjs'],
+    rules: {
+        'no-console': 0,
+        'import/no-unresolved': 0,
+        'linebreak-style': ['error', 'unix'],
+    },
+};
--- next.config.js
+++ next.config.js
@@ -3,17 +3,19 @@ const path = require('path');
 module.exports = {
     // Target must be serverless
     target: 'serverless',
-    webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    // eslint-disable-next-line no-unused-vars
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         // Note: we provide webpack above so you should not `require` it
+        const newConfig = config;
 
         const aliases = config.resolve.alias || {};
-        config.resolve.alias = {
+        newConfig.resolve.alias = {
             ...aliases,
             'private-next-pages': path.resolve(__dirname, 'src/pages'),
             '@': path.resolve(__dirname, 'src'),
         };
 
         // Important: return the modified config
-        return config;
+        return newConfig;
     },
 };
