8bc53081c0fdd71794da31f631f7c267ad615f7f
HaiTS <transachhai97@gmail.com>
2020-10-01 16:39:02 +0700

    nprogress

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 
 import ScrollProgress from '@/components/ScrollProgress/ScrollProgress';
 import LoadingScreen from '@/components/LoadingScreen/LoadingScreen';
+
+// eslint-disable-next-line no-unused-vars
+Router.events.on('routeChangeStart', (url) => {
+    NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function DefaultLayout({ children }) {
     const [spinner, setSpinner] = useState(true);
