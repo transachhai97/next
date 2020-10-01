0b43752743684723828609e0305d4eb239a6cf29
HaiTS <transachhai97@gmail.com>
2020-10-01 14:49:51 +0700

    scss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const withPlugins = require('next-compose-plugins');
+const sass = require('@zeit/next-sass');
 
-module.exports = {
+// eslint-disable-next-line import/no-extraneous-dependencies
+const genericNames = require('generic-names');
+
+const isProduction = process.env.NODE_ENV === 'production';
+const localIdentName = isProduction
+    ? '[name]__[local]--[hash:base64:5]'
+    : '[path][name]__[local]';
+
+const generate = genericNames(localIdentName, {
+    context: process.cwd(),
+});
+
+// eslint-disable-next-line no-shadow
+const getLocalIdent = (loaderContext, localIdentName, localName) =>
+    generate(localName, loaderContext.resourcePath);
+
+const nextConfig = {
     // Target must be serverless
     target: 'serverless',
     // eslint-disable-next-line no-unused-vars
@@ -19,3 +38,42 @@ module.exports = {
         return newConfig;
     },
 };
+
+module.exports = withPlugins(
+    [
+        // add plugins here..
+        [
+            sass,
+            {
+                cssModules: true,
+                cssLoaderOptions: {
+                    importLoaders: 1,
+                    getLocalIdent: (
+                        loaderContext,
+                        // eslint-disable-next-line no-shadow
+                        localIdentName,
+                        localName,
+                        // eslint-disable-next-line no-unused-vars
+                        options
+                    ) => {
+                        const pathLocalName = loaderContext.resourcePath;
+                        if (
+                            pathLocalName.includes('src/styles/index.scss') ||
+                            pathLocalName.includes('src\\styles\\index.scss') ||
+                            pathLocalName.includes('node_modules') ||
+                            localName === 'mode-dark'
+                        ) {
+                            return localName;
+                        }
+                        return getLocalIdent(
+                            loaderContext,
+                            localIdentName,
+                            localName
+                        );
+                    },
+                },
+            },
+        ],
+    ],
+    nextConfig
+);
