6763f6872a8df92398ebca59503fca40e0474bf2
HaiTS <transachhai97@gmail.com>
2020-10-02 16:07:02 +0700

    next seo

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import { PageTransition } from 'next-page-transitions';
+import { DefaultSeo } from 'next-seo';
 
 import ScrollProgress from '@/components/ScrollProgress/ScrollProgress';
 import LoadingScreen from '@/components/LoadingScreen/LoadingScreen';
@@ -10,6 +11,8 @@ import ChangeLanguage from '@/components/ChangeLanguage/ChangeLanguage';
 import DarkModeToggle from '@/components/DarkModeToggle/DarkModeToggle';
 import useOnlineStatus from '@/hooks/useOnlineStatus';
 import Offline from '@/components/Offline/Offline';
+
+import SEO from '@/app/next-seo.config';
 
 // eslint-disable-next-line no-unused-vars
 Router.events.on('routeChangeStart', (url) => {
@@ -39,6 +42,7 @@ export default function DefaultLayout({ children }) {
             <ChangeLanguage />
             <DarkModeToggle />
 
+            <DefaultSeo {...SEO} />
             <PageTransition timeout={300} classNames="page-transition">
                 {children}
             </PageTransition>
